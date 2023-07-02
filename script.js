056f7f6d240aca1f9128409ac98f1450f448e64b
Annie Huang <annie.hn@gmail.com>
2023-07-02 17:54:50 +1000

    Remove function when user click into the <a>, we will drop the the correct tab panel when javascript is off

--- script.js
+++ script.js
@@ -10,3 +10,9 @@ tabButtons.forEach((tab, index) => {
     tabPanels[index].setAttribute('hidden', '');
   }
 });
+
+tabsContainer.addEventListener('click', (e) => {
+  // This is to remove the function when we click the <a>, it will directly jump to the correct tab panel from the href setting.
+  // That one is for when javascript is disabled, but if the code can run into here, it means javascript is enabled. So it is OK to remove the function
+  e.preventDefault();
+});
